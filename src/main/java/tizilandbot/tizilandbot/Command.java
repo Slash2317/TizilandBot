5af30ef2856aa5240722ebffe7ef47682a42e081
Slash2317 <marstheredplanet2@gmail.com>
2023-12-25 22:24:29 +0000

    All Other Commands

--- src/main/java/tizilandbot/tizilandbot/Command.java
+++ src/main/java/tizilandbot/tizilandbot/Command.java
@@ -1,22 +1,43 @@
 package tizilandbot.tizilandbot;
 
+import java.util.Collections;
+import java.util.List;
+
 public enum Command {
 
-    HELP("t!help"),
-    TIZILAND("t!tiziland"),
-    RULES("t!rules"),
-    TIZIPAGES("t!tizipages"),
-    STAFF("t!staff"),
-    ECHO("t!echo");
+    HELP("t!help", "shows list of cmds"),
+    TIZILAND("t!tiziland", "shows invite links to tiziland"),
+    RULES("t!rules", "shows where to find server rules"),
+    TIZIPAGES("t!tizipages", "gives links to all tizi pages"),
+    STAFF("t!staff", "gets the full list of current server staff"),
+    EVENT_START("t!eventstart", "idk"),
+    EVENT_OVER("t!eventover", "idk"),
+    ECHO("t!echo", "says the requested content", List.of("message"));
 
     private final String commandName;
+    private final String description;
+    private final List<String> parameters;
 
-    Command(String commandName) {
+    Command(String commandName, String description, List<String> parameters) {
         this.commandName = commandName;
+        this.description = description;
+        this.parameters = parameters;
+    }
+
+    Command(String commandName, String description) {
+        this(commandName, description, Collections.emptyList());
     }
 
     public String getCommandName() {
         return commandName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public List<String> getParameters() {
+        return parameters;
     }
 
     public static Command getCommand(String message) {
--- src/main/java/tizilandbot/tizilandbot/TizilandBotListener.java
+++ src/main/java/tizilandbot/tizilandbot/TizilandBotListener.java
@@ -3,6 +3,11 @@ package tizilandbot.tizilandbot;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class TizilandBotListener extends ListenerAdapter {
 
     @Override
@@ -13,13 +18,44 @@ public class TizilandBotListener extends ListenerAdapter {
 
         RequestContext requestContext = RequestContext.from(event);
 
-        if (requestContext.getCommand() == Command.HELP) {
-            event.getChannel().sendMessage("oh help yourself").queue();
+        if (requestContext.getCommand() == null) {
+            return;
         }
-        else if (requestContext.getCommand() == Command.TIZILAND) {
-            event.getChannel().sendMessage("The only links to join Tiziland and invite people are:\n" +
-                    ":tiziland_link: Discord Invite Link: https://discord.gg/9XTkWVbycs\n" +
-                    ":tiziland_link: Bit.ly Invite Link: http://bit.ly/tiziland").queue();
+
+        switch (requestContext.getCommand()) {
+            case HELP -> event.getChannel().sendMessage(String.join("\n", getListOfCommands())).queue();
+            case TIZILAND -> event.getChannel().sendMessage("""
+                    The only links to join Tiziland and invite people are:
+                    :tiziland_link: Discord Invite Link: https://discord.gg/9XTkWVbycs
+                    :tiziland_link: Bit.ly Invite Link: http://bit.ly/tiziland""").queue();
+            case RULES -> event.getChannel().sendMessage(":tiziland_rules: You can read our rules here: https://discord.com/channels/1108179404137447484/1108181346033094736").queue();
+            case TIZIPAGES -> event.getChannel().sendMessage("""
+                    Here are all the pages of Tizi:
+                    -bit.ly/tiziabout
+                    -bit.ly/tizisocial
+                    -bit.ly/tizi-links""").queue();
+            case STAFF -> event.getChannel().sendMessage("""
+                    :crown: Owner: Tizi!! (tiziandfrodo)
+                    :tools: Community Manager: Xanth (._.xanth._.)
+                    :sparkles: Admins: N/A
+                    :star2: Mods: Nathan (natxer43) | TireCuzWhyNot (tirecuzwhynot) | Ajax (arsunal)""").queue();
+            case EVENT_START -> event.getChannel().sendMessage("The event just started, come join and participate! :star2:").queue();
+            case EVENT_OVER -> event.getChannel().sendMessage("This event has been closed. Thanks for participating! :heart:").queue();
+            case ECHO -> event.getChannel().sendMessage(requestContext.getParameters()).queue();
         }
+    }
+
+    private List<String> getListOfCommands() {
+        List<String> commandDisplays = new ArrayList<>();
+        for (Command command : Command.values()) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(command.getCommandName());
+            if (!command.getParameters().isEmpty()) {
+                sb.append(" [" + String.join(", ", command.getParameters()) + "]");
+            }
+            sb.append(" (" + command.getDescription() + ")");
+            commandDisplays.add(sb.toString());
+        }
+        return commandDisplays;
     }
 }
